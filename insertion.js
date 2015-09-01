117efc98f81c81046e8849710219fbc09e389974
Matthew Rourke <mrourke@gmail.com>
2015-09-01 13:14:11 -0400

    Add optional comparator parameter

--- insertion.js
+++ insertion.js
@@ -2,12 +2,15 @@ var insertion = function (list, comparator) {
   var i;
   var currentValue;
   var insertIndex;
+  // Optional comparator. Comparator returns true for a precedes b in sort
+  comparator = comparator || function (a, b) {return a < b;};
 
   for (i = 1; i < list.length; i++) {
     currentValue = list[i];
     insertIndex = i;
 
-    while (insertIndex > 0 && currentValue < list[insertIndex - 1]) {
+
+    while (insertIndex > 0 && comparator(currentValue, list[insertIndex - 1])) {
       insertIndex -= 1;
       list[insertIndex + 1] = list[insertIndex];
     }
